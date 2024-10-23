c22874e3e8f309668b6d6099d746b5bbb64df143
GMakarova <galinamakarova3@gmail.com>
2024-10-23 21:36:50 +0000

    edit Class1 files

--- Class1/main.py
+++ Class1/main.py
@@ -1,1 +1,12 @@
 print("Hello World")
+print("I love Python")
+print("My name is Galina")
+
+print(5*5)
+print(5+5*2)
+print(5+5-3*2+5*5)
+print("""
+      Hello, my name is Galina
+      I love DevOps
+      Ilove Python
+      """)
